b50bf319d2ce1f66b242a0c0ecce7f67c6fa9fa4
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-08-28 12:38:46 -0300

    refactor: adjust position of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+// External library
+import { useState } from "react";
+
 // Components
 import Banner from "@components/commons/structure/Banner";
+import TextField from "@components/commons/toolkit/inputs/TextField";
 
 // Styles
 import styles from "./styles.module.css";
 
 function App() {
+  const [name, setName] = useState<string>("");
+
   return (
     <>
       <Banner />
       <main className={styles.main}>
-        <div>teste</div>
-        <div>teste</div>
-        <div>teste</div>
+        <section className={styles.section}>
+          <TextField label="Nome" value={name} changeValue={setName} />
+        </section>
       </main>
     </>
   );
